6f8ec99b2e516aaea0f5db0f4b9f659d18c4be4e
franc1sc0sv <franc1sc0.sv.xd@gmail.com>
2025-02-08 22:16:52 -0600

    feat: add laravel auth api

---
+++ laravel-rest-api/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Bienvenido a la API de Laravel',
+        'status' => 'OK',
+        'timestamp' => now(),
+    ]);
+})->name('api.index');
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+});
