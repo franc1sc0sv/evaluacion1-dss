8072c267dc3f78392189dd7fc795eef9451b3159
ineedoritos <cgon991@gmail.com>
2025-02-26 17:39:58 -0600

    saz-lays

--- laravel-api-rest/app/Models/User.php
+++ laravel-api-rest/app/Models/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable implements JWTSubject
     }
 
     public function getJWTIdentifier()
-    {
+    {   
         return $this->getKey();
     }
     public function getJWTCustomClaims()
